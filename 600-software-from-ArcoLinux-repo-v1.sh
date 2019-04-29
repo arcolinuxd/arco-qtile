39dffd199b7fda7225bb00ef0c8e7a05e040e066
Erik Dubois <erik.dubois@gmail.com>
2019-04-29 11:18:24 +0200

    new package arcolinux-oblogout-qtile

--- 600-software-from-ArcoLinux-repo-v1.sh
+++ 600-software-from-ArcoLinux-repo-v1.sh
@@ -52,8 +52,9 @@ sudo pacman -S arcolinux-mirrorlist-git --noconfirm --needed
 sudo pacman -S arcolinux-neofetch-git --noconfirm --needed
 sudo pacman -S arcolinux-nitrogen-git --noconfirm --needed
 #sudo pacman -S arcolinux-oblogout-awesome --noconfirm --needed
-sudo pacman -S arcolinux-oblogout --noconfirm --needed
+#sudo pacman -S arcolinux-oblogout --noconfirm --needed
 #sudo pacman -S arcolinux-oblogout-bspwm --noconfirm --needed
+sudo pacman -S arcolinux-oblogout-qtile --noconfirm --needed
 sudo pacman -S arcolinux-oblogout-themes-git --noconfirm --needed
 #sudo pacman -S arcolinux-oblogout-xmonad --noconfirm --needed
 #sudo pacman -S arcolinux-obmenu-generator-git --noconfirm --needed

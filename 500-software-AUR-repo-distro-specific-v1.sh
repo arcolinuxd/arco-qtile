3e803c7ebc6eb89237971e891840e725a5b11742
Erik Dubois <erik.dubois@gmail.com>
2019-09-29 16:14:10 +0200

    spotifywm-git added

--- 500-software-AUR-repo-distro-specific-v1.sh
+++ 500-software-AUR-repo-distro-specific-v1.sh
@@ -23,9 +23,11 @@ sh AUR-DS/install-ksuperkey-v*.sh
 
 sh AUR-DS/install-perl-linux-desktopfiles-v*.sh
 
-sh AUR-DS/install-urxvt-resize-font-git-v*.sh
-
 sh AUR-DS/install-python-pywal-v*.sh
+
+sh AUR-DS/install-spotifywm-git-v*.sh
+
+sh AUR-DS/install-urxvt-resize-font-git-v*.sh
 
 echo "################################################################"
 echo "####       Software from AUR-DS folder installed          ######"
